b559a8f42b8bf959cbbb0a1b1ffe6e9986afbf75
rawatnikhil857 <rawatnikhil857@gmail.com>
2023-07-02 00:36:56 +0530

    putting vercel.json in root

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentials: true,
   },
 });
